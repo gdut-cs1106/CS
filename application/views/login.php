d76f037f84630f1fb2617b3feffc85ef4f315102
MrBC <317010856@qq.com>
2014-04-01 20:50:44 +0800

    cao

--- application/views/login.php
+++ application/views/login.php
@@ -11,7 +11,7 @@
 <!--网上找的图片色调不协调，所以用素描处理先把颜色都去掉了，等找到新的合适图片再合成新的背景来代替-->
 <div class="container" id="login_container">
 	<div id="login">
-		<form action="<?=site_url('Home/login'); ?>" method="post">
+		<form action="<?=site_url('Login/login'); ?>" method="post">
 			<h4>Login</h4>
 			<input type="text" name="id" placeholder="ID" />
 			<input type="password" name="pwd" placeholder="Password" /><br />
